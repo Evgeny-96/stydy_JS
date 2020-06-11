0a9615afff7d3f6827febb026e885a01bbc45cbe
Evgeny Panteleev <evg29111@gmail.com>
2020-06-11 23:02:03 +0500

    Закончил урок № 2

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
-alert('Сообщение с любым текстом');
-console.log('Сообщение с любым текстом - 2');
+let money = 30000,
+    income = 'Фриланс',    
+    addExpenses = ('Интернет, Такси, Коммуналка, Проезд'),
+    deposit = true,    
+    mission = 50000,
+    period = 7;
+
+    console.log('money: ', typeof(money));
+    console.log('income: ', typeof(income));
+    console.log('deposit: ', typeof(deposit));
+    console.log('Длина строки addExpenses: ', addExpenses.length);
+    console.log('Период равен (period) месяцев: ', period);
+    console.log('Цель заработать (mission) рублей/долларов/гривен/юани: ', mission);
+    console.log('Вывод массива addExpenses в нижнем регистре: ', addExpenses.toLowerCase().split(', '));
+    
+    let budgetDay = money/30;
+    console.log('budgetDay: ', budgetDay);
